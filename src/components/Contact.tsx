c62ad6b0e2f585d7f896fc74405542487a3630fe
Lahcene <lahcene.tahar@roundrock.io>
2024-01-20 09:12:31 +0100

    update content

---
+++ src/components/Contact.tsx
@@ -0,0 +1,10 @@
+export default function ContactPage() {
+  return (
+    <>
+      <span>
+        30 N Gould St Ste R Sheridan, WY 82801 30 N Gould St, STE 4000 Sheridan.
+        Daytime Phone #: (281) 729-0300
+      </span>
+    </>
+  )
+}
